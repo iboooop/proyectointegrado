8266930251fdb8b3edf02d3f27666e4386d022a9
RafMor26 <rm434308@gmail.com>
2025-11-11 10:38:25 -0300

    cambios en urls.py de sistema_erp

--- sistema_erp/sistema_erp/urls.py
+++ sistema_erp/sistema_erp/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('productos/', include('productos.urls')),
     path('proveedores/', include('proveedores.urls')),
     path('transacciones/', include('transacciones.urls')),
+    path('bodegas/', include('bodegas.urls')),         # <-- Agregado bodegas
+    path('clientes/', include('clientes.urls')),
     # Catch-all: al final, cualquier otra URL muestra el 404 personalizado incluso en DEBUG
     re_path(r'^(?P<extra>.*)$', views.not_found_view, name='not_found'),
 ]
