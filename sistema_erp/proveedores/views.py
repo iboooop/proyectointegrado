1fbbb9082094d4ade987ef68b6487c106019c4e9
RafMor26 <rm434308@gmail.com>
2025-11-11 03:37:32 -0300

    cambios proveedores 1.6

--- sistema_erp/proveedores/views.py
+++ sistema_erp/proveedores/views.py
@@ -3,10 +3,42 @@ from .models import Proveedor
 from productos.models import Producto
 from transacciones.models import MovimientoInventario
 from .forms import ProveedorForm
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 def lista_proveedores(request):
-    proveedores = Proveedor.objects.all().order_by('nombre')
-    return render(request, 'proveedores/proveedor_list.html', {'proveedores': proveedores})
+    q = request.GET.get('q', '')
+    paginate_by = request.GET.get('paginate_by')
+    if not paginate_by:
+        paginate_by = request.session.get('proveedores_paginate_by', 5)
+    else:
+        request.session['proveedores_paginate_by'] = paginate_by
+
+    sort = request.GET.get('sort', 'nombre')
+    dir = request.GET.get('dir', 'asc')
+    order = sort if dir == 'asc' else f'-{sort}'
+
+    proveedores = Proveedor.objects.all()
+
+    if q:
+        proveedores = proveedores.filter(
+            Q(nombre__icontains=q) |
+            Q(rut__icontains=q) |
+            Q(contacto__icontains=q) |
+            Q(telefono__icontains=q) |
+            Q(correo__icontains=q)
+        )
+
+    proveedores = proveedores.order_by(order)
+
+    paginator = Paginator(proveedores, paginate_by)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'proveedores/proveedor_list.html', {
+        'proveedores': page_obj,
+        'page_obj': page_obj,
+    })
 
 
 def crear_proveedor(request):
