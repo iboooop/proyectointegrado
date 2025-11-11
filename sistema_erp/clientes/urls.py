328437d06fa155de95d3c8a9a6859d31607497c5
CatalinaArancibia <arancibia.ca01@gmail.com>
2025-11-11 12:13:56 -0300

    urls

--- sistema_erp/clientes/urls.py
+++ sistema_erp/clientes/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ClienteListView.as_view(), name='clientes_list'),
-    path('create/', views.ClienteCreateView.as_view(), name='clientes_create'),
-    path('<int:pk>/', views.ClienteDetailView.as_view(), name='clientes_detail'),
-    path('<int:pk>/edit/', views.ClienteUpdateView.as_view(), name='clientes_edit'),
-    path('<int:pk>/delete/', views.ClienteDeleteView.as_view(), name='clientes_delete'),
+    path('', views.clientes_list, name='clientes_list'),
+    path('crear/', views.clientes_create, name='clientes_create'),
+    path('<int:pk>/', views.clientes_detail, name='clientes_detail'),
+    path('<int:pk>/editar/', views.clientes_edit, name='clientes_edit'),
+    path('<int:pk>/eliminar/', views.clientes_delete, name='clientes_delete'),
     path('exportar/', views.exportar_clientes_excel, name='clientes_export'),
 ]
