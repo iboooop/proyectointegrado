7643d9b7e0bdc6410b68e042980f2e265df24068
CatalinaArancibia <arancibia.ca01@gmail.com>
2025-11-11 13:00:36 -0300

    urls bodegas y cliente

--- sistema_erp/clientes/urls.py
+++ sistema_erp/clientes/urls.py
@@ -2,15 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # --- LISTAR ---
-    path('', views.bodegas_list, name='bodegas_list'),
-
-    # --- CRUD (todas funciones, como en clientes) ---
-    path('crear/', views.bodegas_create, name='bodegas_create'),
-    path('<int:pk>/', views.bodegas_detail, name='bodegas_detail'),
-    path('<int:pk>/editar/', views.bodegas_edit, name='bodegas_edit'),
-    path('<int:pk>/eliminar/', views.bodegas_delete, name='bodegas_delete'),
-
-    # --- EXPORTAR ---
-    path('exportar/', views.exportar_bodegas_excel, name='bodegas_export'),
+    path('', views.clientes_list, name='clientes_list'),
+    path('crear/', views.clientes_create, name='clientes_create'),
+    path('<int:pk>/', views.clientes_detail, name='clientes_detail'),
+    path('<int:pk>/editar/', views.clientes_edit, name='clientes_edit'),
+    path('<int:pk>/eliminar/', views.clientes_delete, name='clientes_delete'),
+    path('exportar/', views.exportar_clientes_excel, name='clientes_export'),
 ]
